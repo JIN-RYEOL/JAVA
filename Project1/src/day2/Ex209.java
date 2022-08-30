d57d222f7b3eb4a40380557153b5bed76386c185
김진렬 <>
2022-08-30 12:11:16 +0900

    첫번째 커밋

--- Project1/src/day2/Ex209.java
+++ Project1/src/day2/Ex209.java
@@ -2,6 +2,15 @@ package day2;
 
 public class Ex209 {
     public static void main(String[] args) {
-        
+        int num = 5;
+
+        ++num;
+        System.out.println(num); // 6
+        ++num;
+        System.out.println(num); // 7
+        System.out.println(num++); // 7
+        System.out.println(num); // 8
+        System.out.println(++num); // 9
+        System.out.println(num); // 9
     }
 }
